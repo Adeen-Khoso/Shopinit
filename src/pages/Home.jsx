e06e2ee993bb027c2013687ebe5033d0c52694fe
Adeen <adeenkhoso@gmail.com>
2025-01-07 23:34:29 +0500

    Added typescript and some components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Button from "../utility/Button";
+import { Navbar2 } from "../utility/Navbar";
+import { Testimonial33 } from "../utility/Testimonials";
+import { Header78 } from "../utility/Header";
+import { Layout416 } from "../utility/Layout";
 
 const Home = () => {
   return (
     <>
-      <h1 className=" ">this is home page</h1>
+      <Navbar2 />
+      <Header78 />
+      <Layout416 />
+      <Testimonial33 />
       <Button variant="primary" url="/">
         Join
       </Button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "/src/index.css"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "/src/index.css",
+    "./node_modules/@relume_io/relume-ui/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  presets: [require("@relume_io/relume-tailwind")],
   theme: {
     colors: {
       primary_bg: "#FBEAEB",
       secondary_bg: "#FFF5F5",
       primary: "#2F3C7E",
       hov_primary: "#36448a",
-      jett_black: "1B1B1B",
+      jett_black: "#1B1B1B",
       white: "#FFFFFF",
       grey: "#F2F2F2",
     },
@@ -15,3 +21,8 @@ export default {
   },
   plugins: [],
 };
+
+// module.exports = {
+//   content: ["./node_modules/@relume_io/relume-ui/dist/**/*.{js,ts,jsx,tsx}"],
+//   presets: [require("@relume_io/relume-tailwind")],
+// };
