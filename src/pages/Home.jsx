c99b767690ee562fb2163b1e33a34b05f2a129d2
Adeen <adeenkhoso@gmail.com>
2025-01-15 14:00:50 +0500

    Home page complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Testimonial33 } from "../utility/components/Testimonials";
 import { Header78 } from "../utility/components/Header";
+import { Testimonial33 } from "../utility/components/Testimonials";
 import { Layout416 } from "../utility/components/Layout";
 import { Layout408 } from "../utility/components/Layout2";
 import { Banner13 } from "../utility/components/Banner2";
