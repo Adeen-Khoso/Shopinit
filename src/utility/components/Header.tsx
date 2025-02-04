7ef18bc8dee97a5042b77f1a8a0120cd50365903
Adeen <adeenkhoso@gmail.com>
2025-02-04 21:32:12 +0500

    updated

--- src/utility/components/Header.tsx
+++ src/utility/components/Header.tsx
@@ -27,6 +27,7 @@ type Props = {
 
 export type Header78Props = React.ComponentPropsWithoutRef<"section"> & Partial<Props>;
 
+
 export const Header78 = (props: Header78Props) => {
   const { heading, description, buttons, images } = {
     ...Header78Defaults,
