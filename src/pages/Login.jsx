efe83a4695b27d4c877f344eb09428aaf7f35271
Adeen <adeenkhoso@gmail.com>
2025-02-06 00:13:45 +0500

    updated login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Login3 } from "../utility/components/LoginForm";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <Login3 />
+    </>
+  );
 };
 
 export default Login;
