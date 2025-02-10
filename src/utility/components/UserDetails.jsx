6ed62350eda7ea19342d7fe69c66d37ae2df77b1
Adeen <adeenkhoso@gmail.com>
2025-02-10 22:25:04 +0500

    added props support in userDetail

--- src/utility/components/UserDetails.jsx
+++ src/utility/components/UserDetails.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Button } from "@relume_io/relume-ui";
 const buttonStyles = "bg-primary text-white hover:bg-hov_primary";
+import blonde from "../../assets/blonde.jpg";
 
-const UserDetails = () => {
+const UserDetails = ({
+  img = blonde,
+  name = "Alexa Rawles",
+  bio = "Hey👋, This is Alexa here",
+}) => {
   return (
     <>
       <div>
         {/* user pfp */}
         <div>
-          <img src="" alt="" />
+          <img src={img} alt="" />
         </div>
 
         {/* user data */}
         <div>
-          <h3>Alexa Rawles</h3>
-          <p>alexarawles@gmail.com</p>
+          <h3>{name}</h3>
+          <p>{bio}</p>
         </div>
 
         {/* buttons */}
