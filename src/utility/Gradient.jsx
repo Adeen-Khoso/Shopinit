6fbfd866051e7c1a58c2f56e21d6fde87dc1c8ee
Adeen <adeenkhoso@gmail.com>
2025-02-11 13:22:03 +0500

    userdetail ui updated

--- src/utility/Gradient.jsx
+++ src/utility/Gradient.jsx
@@ -5,7 +5,7 @@ const Gradient = () => {
   return (
     <>
       {/* Profile Page Banner */}
-      <div className="w-full h-[9vh] md:h-[14vh]">
+      <div className="w-full h-[10vh] md:h-[18vh]">
         <img
           className="w-full h-full object-cover object-center"
           src={GradientImg}
--- src/utility/components/UserDetails.jsx
+++ src/utility/components/UserDetails.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "@relume_io/relume-ui";
+import { Button, cn } from "@relume_io/relume-ui";
 const buttonStyles = "bg-primary text-white hover:bg-hov_primary";
 import blonde from "../../assets/blonde.jpg";
 
@@ -10,25 +10,31 @@ const UserDetails = ({
 }) => {
   return (
     <>
-      <div className="w-full bg-secondary_bg flex ">
-        {/* user pfp */}
-        <div className="size-14 overflow-hidden rounded-full">
-          <img
-            className="w-full h-full object-cover"
-            src={img}
-            alt="User Profile"
-          />
-        </div>
-
+      <div className="w-[100vw] h-[16vh] md:h-[20vh] bg-secondary_bg border-b rounded-none border-opacity-5 flex justify-between items-center px-[5%] py-[7%] ">
         {/* user data */}
-        <div>
-          <h3>{name}</h3>
-          <p>{bio}</p>
+        <div className="flex gap-4 items-center">
+          <div className="size-14 overflow-hidden rounded-full">
+            <img
+              className="w-full h-full object-cover"
+              src={img}
+              alt="User Profile"
+            />
+          </div>
+          <div className="flex flex-col">
+            <h4 className=" text-xl">{name}</h4>
+            <p className=" text-sm text-background-tertiary">{bio}</p>
+          </div>
         </div>
 
         {/* buttons */}
-        <Button className={buttonStyles}>button</Button>
-        <Button className={buttonStyles}>button</Button>
+        <div className="flex flex-row items-center gap-4">
+          <Button className={cn(buttonStyles)}>button</Button>
+          <Button
+            className={cn(buttonStyles, "bg-secondary_bg text-jett_black")}
+          >
+            button
+          </Button>
+        </div>
       </div>
     </>
   );
