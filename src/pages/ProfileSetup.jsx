a9945de9552834c68ff3f2f0c10e2529ceafc449
Adeen <adeenkhoso@gmail.com>
2025-03-23 04:43:00 +0500

    added toast to profile setup

--- src/pages/ProfileSetup.jsx
+++ src/pages/ProfileSetup.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import ProfileForm from "../utility/components/ProfileForm";
+import toast from "react-hot-toast";
+import { AuthContext } from "../context/AuthContext";
 
 const ProfileSetup = () => {
+  const { user } = useContext(AuthContext);
+
   const onSave = () => {
     console.log("New Profile saved");
   };
+
+  const hasShownToast = useRef(false);
+
+  useEffect(() => {
+    if (user && !hasShownToast.current) {
+      toast("Setup your profile !", {
+        icon: "✎",
+        style: {
+          borderRadius: "0px",
+          background: "#FFF5F5",
+          color: "#333",
+        },
+      });
+      hasShownToast.current = true;
+    }
+  }, [user]);
+
   return (
     <>
       <ProfileForm onSave={onSave} />
