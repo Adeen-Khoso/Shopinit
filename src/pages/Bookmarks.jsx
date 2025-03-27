5942febf1d82fe5fe355c6009a14c1a1a3749795
Adeen <adeenkhoso@gmail.com>
2025-03-27 07:17:46 +0500

    updated bookmarks page ui

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Product4 } from "../utility/components/Product4";
+import { FaHeart } from "react-icons/fa6";
 
 const Bookmarks = () => {
-  return <div>Bookmarks</div>;
+  return (
+    <>
+      <Product4
+        tagline={<FaHeart className=" text-primary size-12 " />}
+        heading="Bookmarks"
+        description="Check out your favorite products"
+        button={{ variant: "secondary", size: "primary", title: "View All" }}
+        // products={productsData} later on, data will be provided using productsData to the children component
+      />
+    </>
+  );
 };
 
 export default Bookmarks;
