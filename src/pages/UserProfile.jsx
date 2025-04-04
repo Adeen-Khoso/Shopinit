c98934d86fa73d85f8b2cf8721c622586cf96ad2
Adeen <adeenkhoso@gmail.com>
2025-04-04 15:08:21 +0500

    updated nodata component

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -4,9 +4,10 @@ import UserDetails from "../utility/components/UserDetails";
 import { Product8 } from "../utility/components/Products";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import NoData from "../utility/NoData";
 
 const UserProfile = () => {
-  const products = [];
+  const userProducts = [];
 
   const productProps = {
     tagline: "Explore",
@@ -17,14 +18,21 @@ const UserProfile = () => {
       size: "primary",
       title: "",
     },
-    products: products,
+    products: userProducts,
   };
   return (
     <>
       <Gradient />
       <UserDetails />
-
-      <Product8 {...productProps} />
+      {userProducts.length === 0 ? (
+        <NoData
+          title="Your Store is Empty"
+          subTitle="Create store and begin selling now with us from "
+          link="/sell"
+        />
+      ) : (
+        <Product8 {...productProps} />
+      )}
     </>
   );
 };
--- src/utility/NoData.jsx
+++ src/utility/NoData.jsx
@@ -5,21 +5,21 @@ import { Link } from "react-router";
 const NoData = ({
   title = "Nothing here yet",
   subTitle = "Get back to shopping from ",
-  link = "/shop",
+  link = "/products",
 }) => {
   return (
-    <div className="px-[5%] -mt-24 md:-mt-32 h-screen flex flex-col justify-center items-center gap-5 text-center ">
+    <div className="px-[5%]  h-[40vh] flex flex-col justify-center items-center gap-5 text-center ">
       <FaInbox className=" text-primary size-12 " />
-      <h1 className="text-3xl  text-jett_black">{title}</h1>
+      <h1 className=" text-2xl md:text-3xl  text-jett_black">{title}</h1>
       <p className="-mt-4">
         {subTitle}
         <Link to={link} className=" underline text-primary cursor-pointer">
           here
         </Link>
-        .
       </p>
     </div>
   );
 };
 
 export default NoData;
+// -mt-24 md:-mt-32
